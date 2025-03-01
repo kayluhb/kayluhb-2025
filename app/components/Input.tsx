4b20f95c105af367d4ae54e04f52d2daef765971
Caleb Brown <killerdillercaleb@gmail.com>
2025-03-01 12:22:55 -0600

    Update some styles for transitions

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -21,7 +21,7 @@ export const Input: React.FC<InputProps> = memo(({ disabled, onChange, onKeyDown
     <input
       aria-description="The text you enter will be displayed as animated particles"
       aria-label="Enter text for particle animation"
-      className="w-[80%] max-w-[300px] rounded-sm border border-gray-300 px-4 py-2 text-center text-lg md:w-auto md:text-base"
+      className="w-[80%] max-w-[300px] rounded-sm border border-gray-300 px-4 py-2 text-center text-lg md:text-base"
       disabled={disabled}
       maxLength={20}
       onBlur={onBlur}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -97,7 +97,7 @@ export default function Index() {
         <div className="sr-only">
           This is an interactive area where you can enter text to see particle animations or upload an image to display.
         </div>
-        <div className="fixed bottom-20 left-1/2 z-10 flex w-full -translate-x-1/2 items-center justify-center gap-2 p-4 md:bottom-6">
+        <div className="fixed bottom-20 left-1/2 z-10 flex w-full -translate-x-1/2 flex-col items-center justify-center gap-2 p-4 md:bottom-6">
           <Input
             onChange={handleWordChange}
             onKeyDown={handleKeyDown}
@@ -109,14 +109,14 @@ export default function Index() {
           {selectedImage ? (
             <button
               onClick={handleCancelImage}
-              className="cursor-pointer rounded-md bg-red-500 p-2 text-white transition-all duration-300 hover:bg-red-600"
+              className="flex w-[80%] max-w-[300px] cursor-pointer justify-center rounded-md bg-red-500 p-2 text-white transition-all duration-300 hover:bg-red-600"
               aria-label="Remove image"
             >
               <XMark className="h-5 w-5" />
             </button>
           ) : (
             <label
-              className="cursor-pointer rounded-md bg-indigo-500 p-2 text-white transition-all duration-300 hover:bg-indigo-600"
+              className="flex w-[80%] max-w-[300px] cursor-pointer justify-center rounded-md bg-indigo-500 p-2 text-white transition-all duration-300 hover:bg-indigo-600"
               role="button"
               tabIndex={0}
               onKeyDown={(e) => {
